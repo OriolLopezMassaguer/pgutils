00e6c215f412583193177900ecda3541a7f13880
Oriol López-Massaguer <oriol.lopez@gmail.com>
2018-11-15 07:38:11 +0100

    duplicate indexes script

---
+++ obtain_dup_indexes.sql
@@ -0,0 +1,20 @@
+SELECT --pg_size_pretty(SUM(pg_relation_size(idx))::BIGINT) AS SIZE,(array_agg(idx))[1] AS idx1
+       'drop index ' ||  (array_agg(idx))[2] || ';' AS idx2
+       --(array_agg(idx))[3] AS idx3, (array_agg(idx))[4] AS idx4
+FROM (
+    SELECT indexrelid::regclass AS idx, (indrelid::text ||E'\n'|| indclass::text ||E'\n'|| indkey::text ||E'\n'||
+                                         COALESCE(indexprs::text,'')||E'\n' || COALESCE(indpred::text,'')) AS KEY
+    FROM pg_index) sub
+GROUP BY KEY HAVING COUNT(*)>1
+ORDER BY SUM(pg_relation_size(idx)) DESC;
+
+
+    SELECT pg_size_pretty(SUM(pg_relation_size(idx))::BIGINT) AS SIZE,
+       (array_agg(idx))[1] AS idx1, (array_agg(idx))[2] AS idx2,
+       (array_agg(idx))[3] AS idx3, (array_agg(idx))[4] AS idx4
+FROM (
+    SELECT indexrelid::regclass AS idx, (indrelid::text ||E'\n'|| indclass::text ||E'\n'|| indkey::text ||E'\n'||
+                                         COALESCE(indexprs::text,'')||E'\n' || COALESCE(indpred::text,'')) AS KEY
+    FROM pg_index) sub
+GROUP BY KEY HAVING COUNT(*)>1
+ORDER BY SUM(pg_relation_size(idx)) DESC;
