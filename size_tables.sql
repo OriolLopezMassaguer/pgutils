8903bb8da56d6a824afcb09e9acaf81110ce7224
Oriol López Massaguer <oriol.lopez@gmail.com>
2019-01-17 10:13:46 +0100

    size tables function

--- size_tables.sql
+++ size_tables.sql
@@ -16,8 +16,8 @@ SELECT *, pg_size_pretty(total_bytes) AS total
 ) a 
 where table_name like 'tb_%' 
 --order by total_bytes desc
---order by row_estimate desc;
-order by table_name asc;
+order by row_estimate desc;
+--rder by table_name asc;
 
 
    
@@ -30,3 +30,42 @@ order by table_name asc;
   ORDER BY pg_relation_size(C.oid) DESC
   LIMIT 20;
 
+SELECT
+  pgClass.relname   AS tableName,
+  pgClass.reltuples AS rowCount
+FROM
+  pg_class pgClass
+LEFT JOIN
+  pg_namespace pgNamespace ON (pgNamespace.oid = pgClass.relnamespace)
+WHERE
+  pgNamespace.nspname NOT IN ('pg_catalog', 'information_schema') AND
+  pgClass.relkind='r'
+order by tableName
+
+
+
+create or replace function 
+count_rows(schema text, tablename text) returns integer
+as
+$body$
+declare
+  result integer;
+  query varchar;
+begin
+  query := 'SELECT count(1) FROM ' || schema || '.' || tablename;
+  execute query into result;
+  return result;
+end;
+$body$
+language plpgsql;
+
+
+select 
+  table_schema,
+  table_name, 
+  count_rows(table_schema, table_name)
+from information_schema.tables
+where 
+  table_schema not in ('pg_catalog', 'information_schema') 
+  and table_type='BASE TABLE'
+order by 3 desc
