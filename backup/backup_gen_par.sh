fab7c60d8d3775c3bedb7be5382be8881cedd60e
oriol <oriol.lopez@chemotargets.com>
2019-07-16 07:40:54 +0200

    port correction

--- backup/backup_gen_par.sh
+++ backup/backup_gen_par.sh
@@ -1,1 +1,1 @@
-pg_dump -p 5433 -Upostgres --compress=9 -W -h $1 -j $3 -Fd -d $2 -f $1_$2_$(date -d "today" +"%Y%m%d%H")_$4.backupp
+pg_dump -Upostgres --compress=9 -W -h $1 -j $3 -Fd -d $2 -f $1_$2_$(date -d "today" +"%Y%m%d%H")_$4.backupp
