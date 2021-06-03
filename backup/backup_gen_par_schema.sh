6367a7d13d3acf4ad5c70ab435bb69de8782ddc8
Oriol López Massaguer <oriol.lopez@chemotargets.com>
2021-06-03 18:01:10 +0200

    excludeed tmp_* in backups

--- backup/backup_gen_par_schema.sh
+++ backup/backup_gen_par_schema.sh
@@ -2,7 +2,7 @@
 # $2 db
 # $3 jobs
 # $4 schema
-filename=$1_$2_$(date -d "today" +"%Y%m%d%H")_$4.backup
+filename=$1_$2_$4_$(date -d "today" +"%Y%m%d%H").backup
 echo $filename
-pg_dump -Upostgres --compress=9 -W -h $1 -j $3 -Fd -d $2 -n $4 -f $1_$2_$(date -d "today" +"%Y%m%d%H")_$4.backup
+pg_dump -Upostgres --compress=9 -T 'tmp_*' -W -h $1 -j $3 -Fd -d $2 -n $4 -f $filename
 chmod -R 770 $filename
